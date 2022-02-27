fcf5e4762d09d9ed47b8cb1c945e00481cecb1f8
InFlames <85372327+InFlames513@users.noreply.github.com>
2022-02-27 17:39:29 +0300

    Delete v13 ekonomi directory

--- v13 ekonomi/başlat.bat
+++
@@ -1,6 +0,0 @@
-echo off
-title Covid-19 Ekonomi bot aktif
-color c
-cls
-:a
-node covid.js
--- v13 ekonomi/slash-command/ping.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: "ping",
-    description: "Botun pingini gösterir.",
-    async execute(interaction, client) {
-      const message = await interaction.fetchReply();
-      var ping = message.createdTimestamp - interaction.createdTimestamp;
-      await interaction.reply({ content: `> Pingim ${ping}`, ephemeral: true });
-    },
-  };
