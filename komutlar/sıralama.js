baf77c10a7c7a922c7e057ff223191b6e27d0005
InFlames <85372327+InFlames513@users.noreply.github.com>
2022-07-24 17:17:56 +0300

    Update sıralama.js

--- komutlar/sıralama.js
+++ komutlar/sıralama.js
@@ -4,6 +4,20 @@ const fs = require("fs");
 
 exports.run = async (client, message, args) => {
   
+  let sayi = 1
+  
+  let content = client.users.cache.filter(x => (db.fetch(`para_${x.id}`))|| 0)
+  .sort((x,y) => (db.fetch(`para_${y.id}`)|| 0) - (db.fetch(`para_${x.id}`))|| 0)
+  .map((x) => {
+    return `${sayi++} **${x.username}**: ${db.fetch(`para_${x.id}`) || 0}`
+  });
+  
+  const leaderBoardEmbed = new discord.MessageEmbed()
+    .setTitle(`En Çok Parası Olan Kullanıcılar`)
+    .setDescription(`${content.slice(0, 10).join("\n")}`)
+    .setColor("AQUA")
+  await message.channel.send({embeds:[leaderBoardEmbed]});
+  
 };
 exports.conf = {
   enabled: true, //kullanıma açık mı değil mi
